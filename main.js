754c445e374ed17a92d1f9d89378ecf1cc31b2e8
SpelpotatisLovesIdle <50461511+Spelpotatis@users.noreply.github.com>
2021-09-12 20:42:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,60 @@
-var cookies = 0;
+var money = 0;
 var cursors = 0;
+var papers = 0;
 
 
 function save(){
 	var save = {
-		cookies: cookies,
+		money: money,
 		cursors: cursors,
+		papers: papers,
 	}
 	localStorage.setItem("save",JSON.stringify(save));
 }
 
 function load(){
 	var savegame = JSON.parse(localStorage.getItem("save"));
-	if (typeof savegame.cookies != "undefined") cookies = savegame.cookies;
+	if (typeof savegame.money != "undefined") money = savegame.money;
 	if (typeof savegame.cursors != "undefined") cursors = savegame.cursors;
+	if (typeof savegame.papers != "undefined") papers = savegame.papers;
 }
 
 load();
 save();
 
 
-function cookieClick(number){
-	cookies = cookies + number;
-	document.getElementById("cookies").innerHTML = cookies;
+function moneyClick(number){
+	money += number;
+	document.getElementById("money").innerHTML = money;
 };
 
 function buyCursor(){
 	var cursorCost = Math.floor(3 * Math.pow(1.2, cursors));
-	if(cookies >= cursorCost){
-		cursors = cursors + 1;
-		cookies = cookies - cursorCost;
+	if(money >= cursorCost){
+		cursors++;
+		money -= cursorCost;
 		document.getElementById('cursors').innerHTML = cursors;
-		document.getElementById('cookies').innerHTML = cookies;
+		document.getElementById('money').innerHTML = money;
 	};
 	var nextCost = Math.floor(3 * Math.pow(1.2, cursors));
 	document.getElementById('cursorCost').innerHTML = nextCost;
 };
 
+function buyPaper(){
+	var paperCost = Math.floor(250 * Math.pow(1.2, papers));
+	if(money >= paperCost){
+		papers++;
+		money -= paperCost;
+		document.getElementById('papers').innerHTML = papers;
+		document.getElementById('money').innerHTML = money;
+	};
+	var nextPaperCost = Math.floor(250 * Math.pow(1.2, papers));
+	document.getElementById('paperCost').innerHTML = nextPaperCost;
+};
+
 
 window.setInterval(function(){
-	cookieClick(cursors);
+	moneyClick(cursors + (papers*5));
 	save();
 }, 1000);
 
@@ -49,4 +64,6 @@ function deleteSave(){
 	localStorage.removeItem("save");
 	window.location.reload(true);
 }
+
+
 
